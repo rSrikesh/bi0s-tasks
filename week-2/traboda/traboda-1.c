291c6d26ae96aad323f50d4f61a2c96087ea7f2f
R.Srikesh <82164995+rSrikesh@users.noreply.github.com>
2021-05-11 21:54:47 +0530

    Add files via upload

---
+++ week-2/traboda/traboda-1.c
@@ -0,0 +1,23 @@
+# include<stdio.h>
+# include<string.h>
+# include<stdlib.h>
+
+void caesar(char* p_text,int key){
+    int i=0;
+    int cyphervalue;
+    while(p_text[i] != '\0' && i < strlen(p_text)-1){
+        cyphervalue = (((int)p_text[i] -97  + key) % 26 + 97 );
+        printf("%c",(char)cyphervalue);
+        i++;
+    }
+    printf("\n");
+}
+
+int main(){
+    int key = 5;
+    char plaintext[50];
+    printf("enter your plaintext: ");
+    fgets(plaintext, sizeof(plaintext),stdin);
+    printf("your ciphered text is: ");
+    caesar(plaintext,key);
+}
