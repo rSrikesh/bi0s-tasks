97f0767d42e4b9ef29527b6394fe0a1d272ddebe
R.Srikesh <82164995+rSrikesh@users.noreply.github.com>
2021-10-16 18:56:23 +0530

    Delete week-7/Easy directory

--- week-7/Easy/1/script.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = 'http://localhost:8080/login'
-credentials = {'username': 'Kit', 'password': 'Kat'}
-cookies = dict(session_id='YWRtaW4xMjM=')
-r = requests.post(url, data=credentials, cookies=cookies)
-print(r.text[2:6] + " : " + r.text[9:21])
--- week-7/Easy/2.py
+++
@@ -1,2 +0,0 @@
-a = int(input())
-print("Leap Year") if a % 4 == 0 and a % 100 != 0 or a % 400 == 0 else print("Not Leap Year")
